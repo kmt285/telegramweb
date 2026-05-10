004d7244477a30d826319153d377975f9410f11c
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-05-10 16:57:48 +0630

    Delete src/models/WebSession.js

--- src/models/WebSession.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const webSessionSchema = new mongoose.Schema({
-    phoneNumber: { type: String, required: true, unique: true },
-    indexedDbData: { type: String, required: true }, // IndexedDB က Data အကုန်လုံးကို String အနေနဲ့ သိမ်းမည်
-    lastUpdated: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('WebSession', webSessionSchema);
